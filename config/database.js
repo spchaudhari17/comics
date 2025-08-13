fecb2ed7859d8fdb1fa73a2ccff81238a22574e0
shubham-virtualTE <spchaudhari@virtualemployee.com>
2025-08-13 21:11:47 +0530

    updating mongodb url

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/comics');
+// mongoose.connect('mongodb://localhost:27017/comics');
+mongoose.connect('mongodb+srv://shubham:shubham@cluster0.e5aknxd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
 mongoose.connection.once('open', function () {
   console.log("database connected")
