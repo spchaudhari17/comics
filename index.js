85b4faf21158134bad7e2cda791235991526280d
shubham-virtualTE <spchaudhari@virtualemployee.com>
2025-08-25 15:38:46 +0530

    updating index.js

--- index.js
+++ index.js
@@ -8,12 +8,34 @@ const path = require("path");
 const fileUpload = require("express-fileupload");
 const connectToDatabase = require('./config/database.js');
 const app = express();
-var corsOptions = {
-  origin: 'http://localhost:3000', 
-  optionsSuccessStatus: 200
-}
 
-app.use(cors({ origin: "http://localhost:3000" })); 
+// var corsOptions = {
+//   origin: 'http://localhost:3000', 
+//   optionsSuccessStatus: 200
+// }
+
+
+
+// app.use(cors({ origin: "http://localhost:3000" })); 
+
+const allowedOrigins = [
+  'http://localhost:3000',   // local dev
+  'http://13.60.35.222'      // production server IP
+  // 'https://yourdomain.com' // optional domain
+];
+
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin (like curl, Postman)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
+}));
 
 app.get('/', (req, res) => {
   console.log("triggered====> :)")
@@ -24,7 +46,6 @@ app.get('/', (req, res) => {
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-// app.use("/comics", cors(corsOptions), express.static(path.join(__dirname, "comics")));
 app.use(
   "/comics",
   (req, res, next) => {
@@ -38,7 +59,6 @@ app.use(
 
 
 
-app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(fileUpload());
