768ab1109ef02da42b35a64b9d7ad63341d7b0c5
shubham-virtualTE <spchaudhari@virtualemployee.com>
2025-10-13 13:49:46 +0530

    adding the fixes

--- app/models/HardcoreQuiz.js
+++ app/models/HardcoreQuiz.js
@@ -1,14 +1,14 @@
-const mongoose = require("mongoose");
-const { Schema } = mongoose;
-const { ObjectId } = Schema.Types;
+  const mongoose = require("mongoose");
+  const { Schema } = mongoose;
+  const { ObjectId } = Schema.Types;
 
-const HardcoreQuizSchema = new Schema({
-  comicId: { type: ObjectId, ref: "Comic", required: true },
-  user_id: { type: ObjectId, ref: "users" },
-  questions: [{ type: ObjectId, ref: "HardcoreQuizQuestion" }],
-  status: { type: String, enum: ["draft", "published"], default: "draft" },
-  mode: { type: String, default: "hardcore" }, // 👈 flag for identification
-  createdAt: { type: Date, default: Date.now }
-});
+  const HardcoreQuizSchema = new Schema({
+    comicId: { type: ObjectId, ref: "Comic", required: true },
+    user_id: { type: ObjectId, ref: "users" },
+    questions: [{ type: ObjectId, ref: "HardcoreQuizQuestion" }],
+    status: { type: String, enum: ["draft", "published"], default: "draft" },
+    mode: { type: String, default: "hardcore" }, // 👈 flag for identification
+    createdAt: { type: Date, default: Date.now }
+  });
 
-module.exports = mongoose.model("HardcoreQuiz", HardcoreQuizSchema);
+  module.exports = mongoose.model("HardcoreQuiz", HardcoreQuizSchema);
