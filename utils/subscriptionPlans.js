b57766173ccce503b8625139bfcecbf69234b6e1
ajinkyakridemy <ajinkya.kridemy@gmail.com>
2026-03-03 23:31:22 +0530

    changes the plan

--- utils/subscriptionPlans.js
+++ utils/subscriptionPlans.js
@@ -1,30 +1,30 @@
 // config/subscriptionPlans.js
 module.exports = {
   bundle: {
-    price_1SvojF1hJWq07BPoiLXsGdb2: {    // 24.99
+    price_1T6wmqKGzJOFnjXyBIZOJshc: {    // 24.99
       comicsPerWeek: 5,
       studentsLimit: 20,
     },
-    price_1SvokW1hJWq07BPoF5eqF96u: {    // 39.99
+    price_1T6wnaKGzJOFnjXyp4bSkk8m: {    // 39.99
       comicsPerWeek: 10,
       studentsLimit: 50,
     },
-    price_1Svoky1hJWq07BPot9vtsvOV: {  // 59.99
+    price_1T6wnnKGzJOFnjXytLtCaPg3: {  // 59.99
       comicsPerWeek: 20,
       studentsLimit: 100,
     },
   },
 
   dashboard: {
-    price_1SvomW1hJWq07BPoTrxz3Tyb: {    // 5
+    price_1T6wozKGzJOFnjXyD61eBJYK: {    // 4.99
       comicsPerWeek: 0,
       studentsLimit: 20,
     },
-    price_1Svomk1hJWq07BPo9IjJLtZD: {   // 10
+    price_1T6wpFKGzJOFnjXy1u6aLEx0: {   // 9.99
       comicsPerWeek: 0,
       studentsLimit: 50,
     },
-    price_1Svoms1hJWq07BPobzFCz5b8: {   // 20
+    price_1T6wpSKGzJOFnjXyPps9FMUG: {   // 19.99
       comicsPerWeek: 0,
       studentsLimit: 100,
     },
