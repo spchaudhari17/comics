2ef4cd2d60180ecbefabfadfaca6056af328e383
ajinkyakridemy <ajinkya.kridemy@gmail.com>
2026-03-04 15:27:19 +0530

    ching the new plan price id

--- utils/subscriptionPlans.js
+++ utils/subscriptionPlans.js
@@ -1,15 +1,30 @@
 // config/subscriptionPlans.js
 module.exports = {
+  // bundle: {
+  //   price_1T6wmqKGzJOFnjXyBIZOJshc: {    // 24.99
+  //     comicsPerWeek: 5,
+  //     studentsLimit: 20,
+  //   },
+  //   price_1T6wnaKGzJOFnjXyp4bSkk8m: {    // 39.99
+  //     comicsPerWeek: 10,
+  //     studentsLimit: 50,
+  //   },
+  //   price_1T6wnnKGzJOFnjXytLtCaPg3: {  // 59.99
+  //     comicsPerWeek: 20,
+  //     studentsLimit: 100,
+  //   },
+  // },
+
   bundle: {
-    price_1T6wmqKGzJOFnjXyBIZOJshc: {    // 24.99
+    price_1T7C1lKGzJOFnjXyBzb0fvZ6: {    // 4.99
       comicsPerWeek: 5,
       studentsLimit: 20,
     },
-    price_1T6wnaKGzJOFnjXyp4bSkk8m: {    // 39.99
+    price_1T7C1xKGzJOFnjXyq4FXqK1x: {    // 7.99
       comicsPerWeek: 10,
       studentsLimit: 50,
     },
-    price_1T6wnnKGzJOFnjXytLtCaPg3: {  // 59.99
+    price_1T7C28KGzJOFnjXyfdAo2ToJ: {  // 11.99
       comicsPerWeek: 20,
       studentsLimit: 100,
     },
