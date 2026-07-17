82c488227ce506696fbaf5c72c7dcd10387cbddf
ajinkyakridemy <ajinkya.kridemy@gmail.com>
2026-07-17 07:04:38 +0530

    chaning the gemini model

--- helper/geminiImage.js
+++ helper/geminiImage.js
@@ -6,7 +6,8 @@ const ai = new GoogleGenAI({
 
 async function generateGeminiComicImage(prompt) {
   const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash-image",
+    // model: "gemini-2.5-flash-image",
+    model: "gemini-3.1-flash-image",
     contents: [
       {
         role: "user",
