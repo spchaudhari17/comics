f6a30632eed458e83741576b885d31ff07fddf03
ajinkyakridemy <ajinkya.kridemy@gmail.com>
2026-09-07 18:51:49 +0530

    adding the live keys and fixing the issues

--- utils/subscriptionPlans.js
+++ utils/subscriptionPlans.js
@@ -1,9 +1,8 @@
-// config/subscriptionPlans.js
+// this is for test keys 
 
 module.exports = {
 
   bundle: {
-
     // Standard
     price_1T6wmqKGzJOFnjXyBIZOJshc: {    // 24.99
       comicsPerWeek: 5,
@@ -38,7 +37,62 @@ module.exports = {
       studentsLimit: 100,
     },
 
+  },
 
+  dashboard: {
+    price_1T7dOiKGzJOFnjXyWL0P3An7: {    // 4.99
+      comicsPerWeek: 0,
+      studentsLimit: 20,
+    },
+    price_1T7dP6KGzJOFnjXyrAE0ZL0E: {   // 9.99
+      comicsPerWeek: 0,
+      studentsLimit: 50,
+    },
+    price_1T7dPIKGzJOFnjXyfI8vdrc8: {   // 19.99
+      comicsPerWeek: 0,
+      studentsLimit: 100,
+    },
+  },
+};
+
+
+// this is for live server keys
+module.exports = {
+
+  bundle: {
+    // Standard
+    price_1UCNs7KGzJOFnjXy1VvfcTIo: {    // 24.99
+      comicsPerWeek: 5,
+      studentsLimit: 20,
+    },
+
+    price_1UCNtSKGzJOFnjXyXh8r2n6k: {    // 39.99
+      comicsPerWeek: 10,
+      studentsLimit: 50,
+    },
+
+    price_1UCNtuKGzJOFnjXyTQGjAkIp: {  // 74.99
+      comicsPerWeek: 20,
+      studentsLimit: 100,
+    },
+
+    // Starter - $16.49/month
+    price_1Tg6gMKGzJOFnjXy2PgwwAMo: {  // 16.49
+      comicsPerWeek: 5,
+      studentsLimit: 20,
+    },
+
+    // Growth - $25.99/month 
+    price_1Tg6gaKGzJOFnjXyHeKvQNek: { // 25.99
+      comicsPerWeek: 10,
+      studentsLimit: 50,
+    },
+
+    // Pro - $48.75/month
+    price_1Tg6gmKGzJOFnjXyPWJj1paZ: { // 48.75
+      comicsPerWeek: 20,
+      studentsLimit: 100,
+    },
 
   },
 
@@ -56,7 +110,4 @@ module.exports = {
       studentsLimit: 100,
     },
   },
-
-
-
 };
